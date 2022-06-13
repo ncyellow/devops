625fe91db5e7cfedd8f9e47b17d527977cd2c9c0
ncyellow <ncyellow@yandex.ru>
2022-06-13 18:38:39 +0300

    Добавил middleware для сжатия gzip

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ncyellow/devops/internal/server/custom_middleware"
 	"github.com/ncyellow/devops/internal/server/storage"
 )
 
@@ -16,6 +17,7 @@ import (
 func NewRouter(repo storage.Repository) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
+	r.Use(custom_middleware.EncoderGZIP)
 
 	r.Get("/", ListHandler(repo))
 	r.Get("/value/{metricType}/{metricName}", ValueHandler(repo))

4e385bb6e3848d1769ba1b8503589594987a4304
ncyellow <ncyellow@yandex.ru>
2022-09-23 22:49:57 +0300

    добавил тесты для кастомного анализатора + для публичных анализатора

--- cmd/staticlin/main.go
+++ cmd/staticlin/main.go
@@ -1,14 +1,20 @@
-// Package main содержит пользовательский статический анализатор. Где подключены
-// 	1."golang.org/x/tools/go/analysis/passes/printf"
-//	2."golang.org/x/tools/go/analysis/passes/shadow"
-//	3."golang.org/x/tools/go/analysis/passes/structtag"
+// Package main содержит пользовательский multichecker.
+// Где подключены:
+// 	1."golang.org/x/tools/go/analysis/passes/printf" - проверка на корректность различных fmt.Print функций
+//	2."golang.org/x/tools/go/analysis/passes/shadow" - проверка на создание "затененных" переменных
+//	3."golang.org/x/tools/go/analysis/passes/structtag" - проверка тегов для для полей структур
 //  4. из пакета statickcheck все проверки вида SA, ST. QF, S1
-//  5. пользовательский анализатор на использование os.Exit в main
-// Стандартное использование main .\...
-
+//  5. пользовательский анализатор на использование os.Exit в пакете main в функции main
+//  6. Публичный анализатор sqlrows - проверяем корректное использование sqlrow
+//  7. Публичный анализатор forcetypeassert - проверка принудительных проверок типов
+// Стандартное использование аналогично go vet .\...
+// main .\...
 package main
 
 import (
+	"github.com/gostaticanalysis/forcetypeassert"
+	"github.com/gostaticanalysis/sqlrows/passes/sqlrows"
+	"github.com/ncyellow/devops/internal/analysis/exit"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/printf"
@@ -18,8 +24,6 @@ import (
 	"honnef.co/go/tools/simple"
 	"honnef.co/go/tools/staticcheck"
 	"honnef.co/go/tools/stylecheck"
-
-	analysis2 "github.com/ncyellow/devops/internal/analysis"
 )
 
 func main() {
@@ -28,24 +32,24 @@ func main() {
 		printf.Analyzer,
 		shadow.Analyzer,
 		structtag.Analyzer,
-		analysis2.ExitCheckAnalyzer,
+		exit.Analyzer,
+		sqlrows.Analyzer,
+		forcetypeassert.Analyzer,
 	}
 
-	// Как я проверил словарь Analyzers содержит только SA проверки, добавляем все что в нем есть
+	// Добавляем все проверки типа SA
 	for _, v := range staticcheck.Analyzers {
 		myChecks = append(myChecks, v.Analyzer)
 	}
-
-	// Просят добавить несколько добавить - добавим все
+	// Добавляем все проверки типа ST
 	for _, v := range stylecheck.Analyzers {
 		myChecks = append(myChecks, v.Analyzer)
 	}
-
+	// Добавляем все проверки типа ST100
 	for _, v := range simple.Analyzers {
 		myChecks = append(myChecks, v.Analyzer)
 	}
-
-	// Просят добавить несколько добавить - добавим все
+	// Добавляем все проверки типа QF1001
 	for _, v := range quickfix.Analyzers {
 		myChecks = append(myChecks, v.Analyzer)
 	}

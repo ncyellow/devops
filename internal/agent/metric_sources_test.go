7a326e55b8e3df599b257e4aa90f3246042f5b52
ncyellow <ncyellow@yandex.ru>
2022-09-29 23:50:43 +0300

    Исправления замечаний
    1. Удалил по итогу залетевший левый файл
    2. Удалил бесплезный тест config
    3. Добавил проверку приращение счетчика. Проверять наличие всех gauges метрик не стал так как договорились что это избыточно

--- internal/agent/metric_sources_test.go
+++ internal/agent/metric_sources_test.go
@@ -1,8 +1,9 @@
 package agent
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // TestRuntimeSource проверяем что источник возвращает корректное число метрик
@@ -12,7 +13,19 @@ func TestRuntimeSource(t *testing.T) {
 	source.Update()
 	gauges := source.Gauges()
 	counters := source.Counters()
-	assert.Equal(t, len(counters), 1)
+	//! Проверяем что приращение счетчика работает корректно
+	assert.Equal(t, counters["PollCount"], int64(1))
+	//! Проверяем что метрики gauges присутствуют,
+	// но договорились что проверять наличие всех метрик избыточно
+	assert.Equal(t, len(gauges) > 0, true)
+
+	source.Update()
+	gauges = source.Gauges()
+	counters = source.Counters()
+	//! После обновления метрик счетчик увеличился
+	assert.Equal(t, counters["PollCount"], int64(2))
+	//! Проверяем что метрики gauges присутствуют,
+	// но договорились что проверять наличие всех метрик избыточно
 	assert.Equal(t, len(gauges), 28)
 }
 
@@ -21,7 +34,8 @@ func TestPSUtilSource(t *testing.T) {
 	source.Update()
 	gauges := source.Gauges()
 	counters := source.Counters()
-	//! Число метрик зависит от числа процессоров но как минимум одна должна быть
+	//! Число метрик зависит от числа процессоров, но как минимум одна должна быть
 	assert.Equal(t, len(gauges) > 0, true)
+	//! Метрики типа counters отсутствуют у источника данных, PSUtils
 	assert.Equal(t, len(counters), 0)
 }
